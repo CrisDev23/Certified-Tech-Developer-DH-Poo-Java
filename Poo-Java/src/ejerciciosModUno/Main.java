7093f6fa52e0bdc63fdeb484e439a74b37917a1b
CrisHash <salva21ale@gmail.com>
2023-02-11 23:48:39 -0300

    primera prueba del entorno

---
+++ Poo-Java/src/ejerciciosModUno/Main.java
@@ -0,0 +1,9 @@
+package ejerciciosModUno;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mi primer ejemplo ");
+        int num = 10;
+        System.out.println("El valor es: " + num);
+    }
+}
